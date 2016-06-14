3b714c6e744548f8a2fc867c41d9c269c1ca16a7
avenue2199 <n232074s@mail.kyutech.jp>
2016-06-14 12:11:05 +0900

    linked from home to eva_inp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+				<div class="panel-body">
+					You are logged in!
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="panel panel-default">						
+						<div class="panel-heading">Contents</div>
+
+						<div class="panel-body">
+							<a class="btn btn-link" href="eva_inp">評価入力画面</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
