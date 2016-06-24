6252934fbc68be19d83dee8f01a5181f827bac71
matsuhei <n232076r@mail.kyutech.jp>
2016-06-24 13:37:11 +0900

    easy login add

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,24 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+ Route::get('/admin/login', function() {
+       $auth = Auth::guard('admin');
+           $credentials = [
+                   'email' => 'admin1@example.com',
+                           'password' => 'password',
+                               ];
+                                
+                                    return $auth->attempt($credentials) ? 'Admin Success' : 'Admin Failure';
+ });
+  
+  Route::get('/user/login', function() {
+        $auth = Auth::guard('user');
+            $credentials = [
+                    'email' => 'user1@test.com',
+                            'password' => 'password',
+                                ];
+                                 
+                                     return $auth->attempt($credentials) ? 'User Success' : 'User Failure';
+  });
+
