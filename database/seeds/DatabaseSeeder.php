e89ed337cf21a761eaa452d87ac8c74580f10e37
avenue2199 <n232074s@mail.kyutech.jp>
2016-07-14 10:33:42 +0900

    add seeder_users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,20 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Model::unguard();
+
+		//実行対象のSeederクラス
+		$this->call(UsersSeeder::class);
+	}
 }
