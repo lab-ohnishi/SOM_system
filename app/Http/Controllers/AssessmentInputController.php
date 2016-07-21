ddbd75a94a434bd5471a97b3fe123e2054ec0fbd
avenue2199 <n232074s@mail.kyutech.jp>
2016-07-22 08:48:25 +0900

    make assessmentsController

--- app/Http/Controllers/AssessmentInputController.php
+++ app/Http/Controllers/AssessmentInputController.php
@@ -1,9 +1,23 @@
 <?php
 namespace App\Http\Controllers;
 
+use DB;
+use App\Http\Controllers\Controller;
+
 class AssessmentInputController extends Controller {
 	public function index()
 	{
+		$perspectives = DB::table('perspectives')->get();
+		$relations = DB::table('relations')->get();
+		$assessments = DB::table('assessments')->get();
+		$comments = DB::table('comments')->get();
+		$configurations = DB::table('configurations')->get();
+
+		return view('perspectives.assessment_inp')->with('perspectives',$perspectives);
+		return view('relations.assessment_inp')->with('relations',$relations);
+		return view('assessments.assessment_inp')->with('assessments',$assessments);
+		return view('comments.assessment_inp')->with('comments',$comments);
+		return view('configurations.assessment_inp')->with('configurations',$configurations);
 		return view('assessment_inp');
 	}
 }
