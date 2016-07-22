30016e2c0e86d98ae23b0c0657df447e02f72957
vagrant <vagrant@homestead>
2016-07-22 17:17:34 +0000

    admin's view create

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,41 +1,43 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+   |--------------------------------------------------------------------------
+   | Application Routes
+   |--------------------------------------------------------------------------
+   |
+   | Here is where you can register all of the routes for an application.
+   | It's a breeze. Simply tell Laravel the URIs it should respond to
+   | and give it the controller to call when that URI is requested.
+   |
+ */
 
 Route::get('/', function () {
     return view('welcome');
-});
+    });
 
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
- Route::get('/admin/login', function() {
-       $auth = Auth::guard('admin');
-           $credentials = [
-                   'email' => 'admin1@example.com',
-                           'password' => 'password',
-                               ];
-                                
-                                    return $auth->attempt($credentials) ? 'Admin Success' : 'Admin Failure';
- });
-  
-  Route::get('/user/login', function() {
-        $auth = Auth::guard('user');
-            $credentials = [
-                    'email' => 'user1@test.com',
-                            'password' => 'password',
-                                ];
-                                 
-                                     return $auth->attempt($credentials) ? 'User Success' : 'User Failure';
-  });
+Route::get('/admin', 'AdminController@index');
+
+Route::get('/admin/login', function() {
+    $auth = Auth::guard('admin');
+    $credentials = [
+    'email' => 'admin1@example.com',
+    'password' => 'password',
+    ];
+
+    return $auth->attempt($credentials) ? 'Admin Success' : 'Admin Failure';
+    });
+
+Route::get('/user/login', function() {
+    $auth = Auth::guard('user');
+    $credentials = [
+    'email' => 'user1@test.com',
+    'password' => 'password',
+    ];
+
+    return $auth->attempt($credentials) ? 'User Success' : 'User Failure';
+    });
 
---
+++ resources/views/admin_top.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+<div class="raw">
+<h1>admin's page</h1>
+</div>
+</div>
+
+@endsection
