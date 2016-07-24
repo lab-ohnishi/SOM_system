1430c974178f25a8a8603d2fac8da34dd47322cc
avenue2199 <n232074s@mail.kyutech.jp>
2016-07-24 22:47:27 +0900

    modified view

--- app/Http/Controllers/AssessmentInputController.php
+++ app/Http/Controllers/AssessmentInputController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 class AssessmentInputController extends Controller {
 	public function index()
 	{
-		$perspectives = DB::table('perspectives')->get();
+	/*	$perspectives = DB::table('perspectives')->get();
 		$relations = DB::table('relations')->get();
 		$assessments = DB::table('assessments')->get();
 		$comments = DB::table('comments')->get();
@@ -18,6 +18,7 @@ class AssessmentInputController extends Controller {
 		return view('assessments.assessment_inp')->with('assessments',$assessments);
 		return view('comments.assessment_inp')->with('comments',$comments);
 		return view('configurations.assessment_inp')->with('configurations',$configurations);
+	 */
 		return view('assessment_inp');
 	}
 }
--- resources/views/assessment_inp.blade.php
+++ resources/views/assessment_inp.blade.php
@@ -7,20 +7,23 @@
 @section('content')
 <div class="container">
 <h1>第__回相互評価入力</h1>
-
+<div class="row">
 <table class="table table-hover table-bordered">
 <thead>
 	<tr>
-		<th>評価項目</th>
-		<th>詳細</th>
-		<th>1:できていない</th>
-		<th>2:あまりできていない</th>
-		<th>3:少しできている</th>
-		<th>4:できている</th>
+		<div class="col-md-3"><th><div class="text-center">評価項目</div></th></div>
+		<div class="col-md-5"><th><div class="text-center">詳細</div></th></div>
+		<div class="col-md-1"><th>1:できていない</th></div>
+		<div class="col-md-1"><th>2:あまりできていない</th></div>
+		<div class="col-md-1"><th>3:少しできている</th></div>
+		<div class="col-md-1"><th>4:できている</th></div>
 	</tr>
 </thead>
 <tbody>
-
+	<tr>
+		
+		
+	</tr>
 </tbody>
 
 </table>
