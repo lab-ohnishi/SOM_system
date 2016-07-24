6decd93c5988d218e76886fdaee08e6dfa8d3fa9
avenue <n232074s@mail.kyutech.jp>
2016-07-24 12:57:31 +0000

    add table

--- resources/views/assessment_inp.blade.php
+++ resources/views/assessment_inp.blade.php
@@ -6,6 +6,26 @@
 
 @section('content')
 <div class="container">
-<h1>相互評価入力</h1>
+<h1>第__回相互評価入力</h1>
+
+<table class="table table-hover table-bordered">
+<thead>
+	<tr>
+		<th>評価項目</th>
+		<th>詳細</th>
+		<th>1:できていない</th>
+		<th>2:あまりできていない</th>
+		<th>3:少しできている</th>
+		<th>4:できている</th>
+	</tr>
+</thead>
+<tbody>
+
+</tbody>
+
+</table>
+
+
+
 </div>
 @endsection
