b41d8ade364074275138ac1549c0f97f89f6d915
matsuhei <n232076r@mail.kyutech.jp>
2016-08-03 09:33:05 +0900

    admin login,logout add

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,41 +10,24 @@
    | and give it the controller to call when that URI is requested.
    |
  */
+Route::group(['middleware' => ['web']], function () {
+    // admin
+    Route::group(['middleware' => 'guest:admin'], function () {
+        Route::get('/admin/login','AdminAuthController@showLoginForm');
+        Route::post('/admin/login','AdminAuthController@login');
+        });
 
-Route::get('/', function () {
-    return view('welcome');
-    });
+    Route::group(['middleware' => 'auth:admin'], function () {
+        Route::get('/admin', 'AdminHomeController@index');
+        Route::get('/admin/home','AdminHomeController@index');
+        });
 
-Route::auth();
+    Route::get('/admin/logout','AdminAuthController@logout');
 
-Route::get('/home', 'HomeController@index');
+    // user
+    Route::auth();
 
-Route::get('/admin', 'AdminController@index');
+    Route::get('/home', 'HomeController@index');
 
-Route::get('/admin/login',function(){
-
-    //Guardを選択(admin)
-    $auth = Auth::guard('admin');
-
-    //認証
-    if($auth->attempt(['email'=>'admin1@admin.com','password'=>'admin1']))
-    {
-    echo "You are Admin!";
-    }else{
-    echo "You are not Admin!";
-    }
-
-    return;
-
-    });
-
-Route::get('/user/login', function() {
-    $auth = Auth::guard('users');
-    $credentials = [
-    'email' => 'test1@example.com',
-    'password' => 'password',
-    ];
-
-    return $auth->attempt($credentials) ? 'User Success' : 'User Failure';
-    });
-
+    Route::get('/', 'HomeController@index');
+});
